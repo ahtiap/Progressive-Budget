63ddb99acbb2e83e12648303a8c5b5e5e885b3cc
Hachem Said <labsar.ahmed@outlook.fr>
2020-09-13 10:50:33 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const compression = require("compression");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(compression());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(
+  "mongodb://budget-tracker:sullivan25@ds061391.mlab.com:61391/heroku_3pqw264f",
+  {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+  }
+);
+
+// routes
+app.use(require("./routes/api.js"));
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
